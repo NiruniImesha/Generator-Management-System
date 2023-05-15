5be93bf7ce473a3725287a36061dca04d783bf36
Niruni Imesha <niruniimesha@gmail.com>
2023-05-15 15:49:08 +0530

    changes

--- GeneratorManagementSyestem/View/generatorOperations.Designer.cs
+++ GeneratorManagementSyestem/View/generatorOperations.Designer.cs
@@ -45,6 +45,8 @@
             this.cmbGenerator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbGenerator.Enabled = false;
             this.cmbGenerator.FormattingEnabled = true;
+            this.cmbGenerator.Items.AddRange(new object[] {
+            "Select Generator"});
             this.cmbGenerator.Location = new System.Drawing.Point(316, 223);
             this.cmbGenerator.Name = "cmbGenerator";
             this.cmbGenerator.Size = new System.Drawing.Size(214, 21);
