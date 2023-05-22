9b4552f652f47c7f9d873270d21bea10bca4cbef
Lasithachamara <91752710+Lasithachamara@users.noreply.github.com>
2023-05-22 13:27:23 +0530

    Update Dashboard.cs

--- GeneratorManagementSyestem/View/Dashboard.cs
+++ GeneratorManagementSyestem/View/Dashboard.cs
@@ -1,4 +1,6 @@
-﻿using GeneratorManagementSyestem.View;
+﻿using GeneratorManagementSyestem.Controller;
+using GeneratorManagementSyestem.Model;
+using GeneratorManagementSyestem.View;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +15,23 @@ namespace GeneratorManagementSyestem
 {
     public partial class Dashboard : Form
     {
-        public Dashboard(string userName)
+        userController uCon = new userController();
+        userModel uMod = new userModel();
+
+        public Dashboard(string userName, string Password)
         {
             InitializeComponent();
             string user = userName;
+            string Pass = Password;
+            string user_id;
+
+            uMod.UserName = user;
+            uMod.Password = Pass;
+
+            user_id  = uCon.findUserID(uMod);
+            uMod.UserID_main = user_id;
+
+            Name_label2.Text = uMod.UserName;
         }
         
         private void btnGenView_Click(object sender, EventArgs e)
@@ -42,7 +57,7 @@ namespace GeneratorManagementSyestem
 
         private void btnAddServices_Click(object sender, EventArgs e)
         {
-            InsertServiceHistory newChild = new InsertServiceHistory();
+            InsertServiceHistory newChild = new InsertServiceHistory(uMod.UserID_main);
             newChild.MdiParent = this;
             newChild.Show();
         }
