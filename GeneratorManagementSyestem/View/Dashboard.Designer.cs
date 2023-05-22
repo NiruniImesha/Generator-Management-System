bb9177de86b12b899f3ccc558800822c7f8b3bc3
Lasithachamara <91752710+Lasithachamara@users.noreply.github.com>
2023-05-22 13:27:20 +0530

    Update Dashboard.Designer.cs

--- GeneratorManagementSyestem/View/Dashboard.Designer.cs
+++ GeneratorManagementSyestem/View/Dashboard.Designer.cs
@@ -29,14 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAddUser = new System.Windows.Forms.Button();
             this.btnAddServices = new System.Windows.Forms.Button();
             this.btnGenView = new System.Windows.Forms.Button();
             this.btnDailyUsage = new System.Windows.Forms.Button();
             this.btnAddGeneratorActions = new System.Windows.Forms.Button();
             this.btnHistory = new System.Windows.Forms.Button();
             this.add_generator = new System.Windows.Forms.Button();
-            this.btnAddUser = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Name_label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -54,6 +58,19 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1413, 88);
             this.panel1.TabIndex = 4;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.BackColor = System.Drawing.Color.Ivory;
+            this.btnAddUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAddUser.Font = new System.Drawing.Font("Calisto MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddUser.Location = new System.Drawing.Point(1182, 23);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(170, 43);
+            this.btnAddUser.TabIndex = 6;
+            this.btnAddUser.Text = "Add user";
+            this.btnAddUser.UseVisualStyleBackColor = false;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
             // 
             // btnAddServices
             // 
@@ -133,18 +150,37 @@
             this.add_generator.UseVisualStyleBackColor = false;
             this.add_generator.Click += new System.EventHandler(this.add_generator_Click);
             // 
-            // btnAddUser
+            // panel3
             // 
-            this.btnAddUser.BackColor = System.Drawing.Color.Ivory;
-            this.btnAddUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnAddUser.Font = new System.Drawing.Font("Calisto MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddUser.Location = new System.Drawing.Point(1182, 23);
-            this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(170, 43);
-            this.btnAddUser.TabIndex = 6;
-            this.btnAddUser.Text = "Add user";
-            this.btnAddUser.UseVisualStyleBackColor = false;
-            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            this.panel3.BackColor = System.Drawing.Color.Cyan;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.Name_label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 642);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1403, 67);
+            this.panel3.TabIndex = 6;
+            // 
+            // Name_label2
+            // 
+            this.Name_label2.AutoSize = true;
+            this.Name_label2.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name_label2.Location = new System.Drawing.Point(153, 26);
+            this.Name_label2.Name = "Name_label2";
+            this.Name_label2.Size = new System.Drawing.Size(53, 19);
+            this.Name_label2.TabIndex = 1;
+            this.Name_label2.Text = " Name ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name :- ";
             // 
             // Dashboard
             // 
@@ -153,12 +189,15 @@
             this.BackgroundImage = global::GeneratorManagementSyestem.Properties.Resources.abstract_gdc5af2023_1920;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1403, 709);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.IsMdiContainer = true;
             this.Name = "Dashboard";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -173,6 +212,9 @@
         private System.Windows.Forms.Button btnGenView;
         private System.Windows.Forms.Button btnAddServices;
         private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label Name_label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
